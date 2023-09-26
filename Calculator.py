8dac64fe5064ce0279b4bba285f2be65598253bf
Mainulll <minullokuliyana@hotmail.com>
2023-09-26 21:50:12 +1000

    New comments added

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,9 @@
+# testing using While loops
+# loop basically checks for conditions here; as Python typcasts inputs as str
 while True:
-    try:
-        FirstNum = int(input("Enter a number: "))
+    try:      
+        FirstNum = int(input("Enter a number: "))  #while FirstNum is an int is True, loop will break
         break
-    except ValueError:
-        print ('Try Again')
+    except ValueError: #Excpet for ValueError, if anything except for a String is entered
+        print ('Try Again')  #Will print this and loop back to Start!
 
